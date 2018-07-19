f4930e8e5ef08bafc0e9e4afdf85e14fa561ce89
shelleywu <shelleywu12@gmail.com>
2018-07-19 16:32:34 -0700

    push content

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-var airtable_list_url = 'https://api.airtable.com/v0/appQ3KIrYyj9fUk3z/Applicants?api_key=keyCUEDL2QDeiT6a1'
+var airtable_list_url = 'https://api.airtable.com/v0/appT2loD1k3FTMUYx/Table%201?api_key=keyCUEDL2QDeiT6a1';
 
 
 // This is where we get the JSON data from Airtable!
 
 $.getJSON( airtable_list_url, function( data ) {
-    var items = [];
-    $.each( data.records, function( key, val ) {
-      console.log(val.fields)
-      items.push(`<h2>${val.fields['Name']}</h2>`);
-    });
-    $(".list-view").append(items.join(''));
+  var items = [];
+  $.each( data.records, function( key, val ) {
+    console.log(val.fields)
+    items.push(`<h2>${val.fields['Name']}</h2>`);
   });
+  $(".list-view").append(items.join(''));
+});
